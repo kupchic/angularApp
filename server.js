b2c39f6f22ec5228a8235d9c816f975a38109ea1
kupchic <kup4enya1999@gmail.com>
2021-10-31 17:01:28 +0300

    fixes

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static("./dist/bookmarks"));
 
-app.get("/*", (req, res) => res.sendFile("index.html", {root: "dist/bookmarks/"}));
+app.get("/*", function (req, res) {
+	res.sendFile(path.join(__dirname + "/dist/bookmarks/index.html"));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
