e7675e4ebba1eff64c777d9c8efc5c9cdbce2a48
kupchic <kup4enya1999@gmail.com>
2021-10-31 17:15:43 +0300

    fixes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/bookmarks"));
+app.use(express.static(__dirname + "/dist/bookmarks"));
 
 app.get("/*", function (req, res) {
 	res.sendFile(path.join(__dirname + "/dist/bookmarks/index.html"));
